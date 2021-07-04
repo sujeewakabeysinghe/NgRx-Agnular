25e88bef96cbdbe91986303b08bca11982dea41e
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2021-07-04 19:01:15 +0530

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { CustomerModule } from './customer/customer.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
